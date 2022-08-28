84c272a1197dd13200bfb255023e05cf9ea3e5f1
Xun6 <13617920171@163.com>
2022-08-28 16:40:44 +0800

    添加显示等待，处理弹窗问题

--- uitest_xueqiu/src/main/java/SampleTest.java
+++ uitest_xueqiu/src/main/java/SampleTest.java
@@ -1,18 +1,25 @@
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.functions.AppiumFunction;
 import io.appium.java_client.remote.options.BaseOptions;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class SampleTest {
     private AndroidDriver driver;
@@ -28,6 +35,8 @@ public class SampleTest {
         baseOptions.setCapability("appActivity", ".view.WelcomeActivityAlias");
         baseOptions.setCapability("autoGrantPermissions", "true");
         baseOptions.setCapability("unicodeKeyboard","true"); //使用unicode编码的键盘，可输入中文
+        baseOptions.setCapability("resetKeyboard","true"); // 将键盘重置为原始状态
+        baseOptions.setCapability("noReset","true"); //不停止应用，不清除数据
 
         URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
@@ -37,6 +46,23 @@ public class SampleTest {
 
     @Test
     public void sampleTest() {
+        // 添加显示等待,超时时间5秒, TODO 用来灵活处理弹窗
+        WebDriverWait webDriverWait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        // 等待直到获取元素或抛出异常
+        try{
+            WebElement element = webDriverWait.until(new AppiumFunction<WebDriver, WebElement>() {
+                @Override
+                public WebElement apply(WebDriver input) {
+                    return input.findElement(By.id("com.xueqiu.android:id/tv_agree"));
+                }
+            });
+            System.out.println(element.isDisplayed());
+            element.click();
+        } catch (Exception e){
+            System.out.println("捕获超时异常，没有找到弹窗元素！");
+            e.printStackTrace();
+        }
+
         // TODO MobileElement对象被弃用，
         //  详情见：https://github.com/appium/java-client/blob/master/docs/v7-to-v8-migration-guide.md
         WebElement el3 = driver.findElement(By.id("com.xueqiu.android:id/home_search"));
@@ -51,6 +77,22 @@ public class SampleTest {
         args.put("y", 200);
         driver.executeScript("mobile: doubleClickGesture", args);
 
+    }
+
+    @Test
+    public void test_performance(){
+        // 返回支持读取的系统状态信息类型，如cpu、内存、网络流量和电池
+        List<String> performanceTypes = driver.getSupportedPerformanceDataTypes();
+        System.out.println("------输出系统状态信息类型--------");
+        for (String per : performanceTypes){
+            try{
+                // 返回支持读取的系统状态具体信息，如cpu、内存、网络流量和电池
+                List<List<Object>> performanceData = driver.getPerformanceData("com.xueqiu.android", per, 5);
+                System.out.println(performanceData.toString());
+            }catch (Exception e){
+                System.out.println(per.toString());
+            }
+        }
     }
 
     @AfterTest
